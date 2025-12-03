fff7d2a020a559fc88f09a551e97acfe03916d78
Emanuel Ferm <emanuel@eferm.com>
2025-12-03 13:56:00 -0800

    cleanup

--- src/day02.ts
+++ src/day02.ts
@@ -1,25 +1,18 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("inputs/day02.txt", "utf-8").trim();
-// const input =
-//   "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
-//   "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
-//   "824824821-824824827,2121212118-2121212124".trim();
-// console.log(input);
 
 function part1(input: string) {
   return input
     .split(",")
     .map((range) => range.split("-").map(Number) as [number, number])
     .flatMap(([first, last]) => {
-      return Array.from(
-        { length: last - first + 1 },
-        (_, i) => first + i,
-      ).filter((i) => {
-        const text = String(i);
-        const mid = Math.floor(text.length / 2);
-        return text.slice(0, mid).repeat(2) === text;
-      });
+      return Array.from({ length: last - first + 1 }, (_, i) => first + i);
+    })
+    .filter((i) => {
+      const text = String(i);
+      const mid = Math.floor(text.length / 2);
+      return text.slice(0, mid).repeat(2) === text;
     })
     .reduce((sum, num) => sum + num, 0);
 }
