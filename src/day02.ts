33274de2c020a31c576cf1d0362509bbce7b1292
Emanuel Ferm <emanuel@eferm.com>
2025-12-03 13:52:11 -0800

    day 2 part 1

---
+++ src/day02.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("inputs/day02.txt", "utf-8").trim();
+// const input =
+//   "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
+//   "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+//   "824824821-824824827,2121212118-2121212124".trim();
+// console.log(input);
+
+function part1(input: string) {
+  return input
+    .split(",")
+    .map((range) => range.split("-").map(Number) as [number, number])
+    .flatMap(([first, last]) => {
+      return Array.from(
+        { length: last - first + 1 },
+        (_, i) => first + i,
+      ).filter((i) => {
+        const text = String(i);
+        const mid = Math.floor(text.length / 2);
+        return text.slice(0, mid).repeat(2) === text;
+      });
+    })
+    .reduce((sum, num) => sum + num, 0);
+}
+
+function part2(input: string) {
+  return 0;
+}
+
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
