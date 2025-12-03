2fdf02710ffe0a0b8e884428b7a8f48d400c0c76
Emanuel Ferm <emanuel@eferm.com>
2025-12-02 23:44:14 -0800

    day 1 part 2

--- src/day01.ts
+++ src/day01.ts
@@ -1,19 +1,8 @@
 import { readFileSync } from 'fs';
 
 const input = readFileSync('inputs/day01.txt', 'utf-8').trim();
-// const input = `
-// L68
-// L30
-// R48
-// L5
-// R60
-// L55
-// L1
-// L99
-// R14
-// L82`.trim()
 
-// console.log(input)
+
 
 function part1(input: string) {
     const positions = input
@@ -28,12 +17,27 @@ function part1(input: string) {
             next = ((next % 100) + 100) % 100;
             return [...acc, next];
         }, [50]);
-    console.log(positions)
+    // console.log(positions)
     return positions.filter(n => n === 0).length
 }
 
 function part2(input: string) {
-    return 0;
+    const [, zeros] = input
+        .split('\n')
+        .map(line => [
+            line.match(/[A-Z]/)![0],
+            parseInt(line.match(/\d+/)![0])
+        ] as [string, number])
+        .reduce<[number, number]>((acc, [dir, num]) => {
+            let [next, zero] = acc;
+            const delta = dir === "L" ? -1 : 1;
+            for (let i = 0; i < num;  i++) {
+                next = ((next + delta) % 100 + 100) % 100;
+                if (next === 0) zero++;
+            }
+            return [next, zero];
+        }, [50, 0]);
+    return zeros
 }
 
 console.log('Part 1:', part1(input));
