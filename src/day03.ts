0dd1743c0833b4c4fbc2f7773b125fb2bbb7c6ec
Emanuel Ferm <emanuel@eferm.com>
2025-12-04 23:06:45 -0800

    day 3 part 1

---
+++ src/day03.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("inputs/day03.txt", "utf-8").trim();
+// const input = `
+// 987654321111111
+// 811111111111119
+// 234234234234278
+// 818181911112111
+// `.trim();
+
+function part1(input: string) {
+  return input
+    .split("\n")
+    .map((bank) => Array.from(bank).map(Number))
+    .map((bank) => {
+      const lmax = Math.max(...bank.slice(0, bank.length - 1));
+      const lidx = bank.indexOf(lmax);
+      const rmax = Math.max(...bank.slice(lidx + 1, bank.length));
+      return Number(`${lmax}${rmax}`);
+    })
+    .reduce((sum, num) => sum + num, 0);
+}
+
+function part2(input: string) {
+  return 0;
+}
+
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
