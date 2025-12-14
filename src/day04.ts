c1e93b25e68f136e26d2cd25c280eed7e0746234
Emanuel Ferm <emanuel@eferm.com>
2025-12-14 14:43:35 -0500

    day 4 part 2

--- src/day04.ts
+++ src/day04.ts
@@ -15,27 +15,41 @@ const input = readFileSync("inputs/day04.txt", "utf-8").trim();
 // `.trim();
 // console.log(input);
 
+function update(grid: string[][]): string[][] {
+  return grid.map((row, r) =>
+    row.map((val, c) => {
+      if (val === "@") {
+        return grid
+          .slice(Math.max(r - 1, 0), r + 2)
+          .flatMap((row) => row.slice(Math.max(c - 1, 0), c + 2))
+          .filter((x) => x === "@").length <= 4
+          ? "x"
+          : val;
+      }
+      return val;
+    }),
+  );
+}
+
 function part1(input: string): number {
   const grid = input.split("\n").map((row) => row.split(""));
-  return grid
-    .flatMap((row, r) =>
-      row.map((val, c) => {
-        if (val === "@") {
-          return grid
-            .slice(Math.max(r - 1, 0), r + 2)
-            .flatMap((row) => row.slice(Math.max(c - 1, 0), c + 2))
-            .filter((x) => x === "@").length <= 4
-            ? "x"
-            : val;
-        }
-        return val;
-      }),
-    )
+  return update(grid)
+    .flat()
     .filter((x) => x === "x").length;
 }
 
+function equals(a: string[][], b: string[][]): boolean {
+  return a.every((row, r) => row.every((val, c) => val === b[r]![c]));
+}
+
 function part2(input: string): number {
-  return 0;
+  let grid = input.split("\n").map((row) => row.split(""));
+  while (true) {
+    const next = update(grid);
+    if (equals(grid, next)) break;
+    grid = next;
+  }
+  return grid.flat().filter((x) => x === "x").length;
 }
 
 console.log("Part 1:", part1(input));
