cc91940631ad2b1ccf515d58b45bac09ea569a9c
Emanuel Ferm <emanuel@eferm.com>
2025-12-15 16:20:06 -0500

    cleanup

--- src/day05.ts
+++ src/day05.ts
@@ -1,19 +1,6 @@
 import { readFileSync } from "node:fs";
 
 const input = readFileSync("inputs/day05.txt", "utf-8").trim();
-// const input = `
-// 3-5
-// 10-14
-// 16-20
-// 12-18
-
-// 1
-// 5
-// 8
-// 11
-// 17
-// 32`.trim();
-// console.log(input, "\n--");
 
 function part1(input: string): number {
   const [fresh, avail] = input
@@ -34,8 +21,9 @@ function part2(input: string): number {
     .sort(([a], [b]) => a - b)
     .reduce(
       ([sum, max], [from, to]) => {
-        const incr = Math.max(to - Math.max(from, max) + 1, 0);
-        return [sum + incr, Math.max(to + 1, max)];
+        return to < max
+          ? [sum, max]
+          : [sum + (to - Math.max(from, max) + 1), to + 1];
       },
       [0, 0],
     )
