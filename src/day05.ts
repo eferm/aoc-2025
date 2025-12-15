5a3a350d836e5e1a8fcca62f4a59eb365eedd002
Emanuel Ferm <emanuel@eferm.com>
2025-12-15 16:32:33 -0500

    cleanup

--- src/day05.ts
+++ src/day05.ts
@@ -13,21 +13,25 @@ function part1(input: string): number {
 }
 
 function part2(input: string): number {
-  return input
-    .split("\n\n")
-    .at(0)!
-    .split("\n")
-    .map((range) => range.split("-").map(Number) as [number, number])
-    .sort(([a], [b]) => a - b)
-    .reduce(
-      ([sum, max], [from, to]) => {
-        return to < max
-          ? [sum, max]
-          : [sum + (to - Math.max(from, max) + 1), to + 1];
-      },
-      [0, 0],
-    )
-    .at(0)!;
+  return (
+    input
+      .split("\n\n")
+      .at(0)!
+      .split("\n")
+      .map((range) => range.split("-").map(Number) as [number, number])
+      // sort to process left-most boundary first, then can truncate next left
+      // boundary with previous right boundary
+      .sort(([a], [b]) => a - b)
+      .reduce(
+        ([sum, max], [from, to]) => {
+          return to < max
+            ? [sum, max] // can skip since we have already processed up to max
+            : [sum + (to - Math.max(from, max) + 1), to + 1];
+        },
+        [0, 0], // (total fresh, maximum right boundary seen)
+      )
+      .at(0)!
+  );
 }
 
 console.log("Part 1:", part1(input));
