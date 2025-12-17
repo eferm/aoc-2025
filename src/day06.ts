5945542561eb52f4e61b828f210c330c5b6712a4
Emanuel Ferm <emanuel@eferm.com>
2025-12-16 20:47:44 -0500

    cleanup

--- src/day06.ts
+++ src/day06.ts
@@ -1,11 +1,6 @@
 import { readFileSync } from "node:fs";
 
 const input = readFileSync("inputs/day06.txt", "utf-8").slice(0, -1);
-// const input = `123 328  51 64
-//  45 64  387 23
-//   6 98  215 314
-// *   +   *   +  `;
-console.log(input);
 
 const operator = {
   "+": (a: number, b: number) => a + b,
@@ -16,7 +11,7 @@ function part1(input: string): number {
   const rows = input.split("\n").map((row) => row.trim().split(/\s+/));
   const [nums, ops] = [
     rows.slice(0, -1).map((row) => row.map(Number)),
-    rows.slice(-1).flat() as ("+" | "*")[],
+    rows.at(-1) as ("+" | "*")[],
   ];
   return ops
     .map((op, i) =>
@@ -29,31 +24,33 @@ function part1(input: string): number {
 
 function part2(input: string): number {
   const rows = input.split("\n").map((row) => row.split(""));
-  return rows
-    .at(-1)!
-    .map<["+" | "*" | null, number]>((char, i) => [
-      char === "+" || char === "*" ? char : null,
-      +rows
-        .slice(0, -1)
-        .map((row) => row.slice(i, i + 1))
-        .flat()
-        .join(""),
-    ])
-    .reduce(
-      (acc, [op, val]) => {
-        if (op === "+" || op === "*") {
-          // start a new group
-          acc.push([op, [val]]);
-        } else if (val > 0) {
-          // push into the last group's number list
-          acc.at(-1)?.[1].push(val);
-        }
-        return acc;
-      },
-      [] as ["+" | "*", number[]][],
-    )
-    .map(([op, vals]) => vals.reduce((sum, val) => operator[op](sum, val)))
-    .reduce((sum, val) => sum + val);
+  const [nums, ops] = [rows.slice(0, -1), rows.at(-1)!];
+  return (
+    ops
+      // create (operator, vertical number) tuples from columns
+      .map<["+" | "*" | null, number]>((char, i) => [
+        char === "+" || char === "*" ? char : null,
+        +nums
+          .map((row) => row.at(i)) // get i:th column
+          .flat()
+          .join(""),
+      ])
+      // restructure into (operator + list of numbers) tuples
+      .reduce(
+        (acc, [op, val]) => {
+          if (op === "+" || op === "*") {
+            acc.push([op, [val]]); // start a new group
+          } else if (val > 0) {
+            acc.at(-1)?.[1].push(val); // push into the last group's list
+          }
+          return acc;
+        },
+        [] as ["+" | "*", number[]][],
+      )
+      // apply operator onto list of numbers
+      .map(([op, vals]) => vals.reduce((sum, val) => operator[op](sum, val)))
+      .reduce((sum, val) => sum + val)
+  );
 }
 
 console.log("Part 1:", part1(input));
