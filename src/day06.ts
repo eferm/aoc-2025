26bb5f37a720a4a1693013cef194492c5e5b9d56
Emanuel Ferm <emanuel@eferm.com>
2025-12-16 20:29:51 -0500

    day 6 part 2

--- src/day06.ts
+++ src/day06.ts
@@ -1,12 +1,11 @@
 import { readFileSync } from "node:fs";
 
-const input = readFileSync("inputs/day06.txt", "utf-8").trim();
-// const input = `
-// 123 328  51 64
+const input = readFileSync("inputs/day06.txt", "utf-8").slice(0, -1);
+// const input = `123 328  51 64
 //  45 64  387 23
 //   6 98  215 314
-// *   +   *   +  `.trim();
-// console.log(input);
+// *   +   *   +  `;
+console.log(input);
 
 const operator = {
   "+": (a: number, b: number) => a + b,
@@ -29,7 +28,32 @@ function part1(input: string): number {
 }
 
 function part2(input: string): number {
-  return 0;
+  const rows = input.split("\n").map((row) => row.split(""));
+  return rows
+    .at(-1)!
+    .map<["+" | "*" | null, number]>((char, i) => [
+      char === "+" || char === "*" ? char : null,
+      +rows
+        .slice(0, -1)
+        .map((row) => row.slice(i, i + 1))
+        .flat()
+        .join(""),
+    ])
+    .reduce(
+      (acc, [op, val]) => {
+        if (op === "+" || op === "*") {
+          // start a new group
+          acc.push([op, [val]]);
+        } else if (val > 0) {
+          // push into the last group's number list
+          acc.at(-1)?.[1].push(val);
+        }
+        return acc;
+      },
+      [] as ["+" | "*", number[]][],
+    )
+    .map(([op, vals]) => vals.reduce((sum, val) => operator[op](sum, val)))
+    .reduce((sum, val) => sum + val);
 }
 
 console.log("Part 1:", part1(input));
